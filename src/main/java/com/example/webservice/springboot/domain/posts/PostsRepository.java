da51ec3c8557b82366613ee167744a9e8ca23d59
김민훈 <kmh357@tong.com>
2021-03-15 14:06:35 +0900

    post pages add

--- src/main/java/com/example/webservice/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/example/webservice/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,15 @@
 package com.example.webservice.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    @Query("SELECT p From Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/example/webservice/springboot/service/posts/PostsService.java
+++ src/main/java/com/example/webservice/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.example.webservice.springboot.service.posts;
 
 import com.example.webservice.springboot.domain.posts.Posts;
 import com.example.webservice.springboot.domain.posts.PostsRepository;
+import com.example.webservice.springboot.web.dto.PostsListResponseDto;
 import com.example.webservice.springboot.web.dto.PostsResponseDto;
 import com.example.webservice.springboot.web.dto.PostsSaveRequestDto;
 import com.example.webservice.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,29 +10,47 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto){
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
