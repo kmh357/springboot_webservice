071328038eee53275633f9a9d4505a55cb91b71c
김민훈 <kmh357@tong.com>
2021-03-16 13:35:22 +0900

    social login

--- src/main/java/com/example/webservice/springboot/web/IndexController.java
+++ src/main/java/com/example/webservice/springboot/web/IndexController.java
@@ -1,22 +1,32 @@
 package com.example.webservice.springboot.web;
 
+import com.example.webservice.springboot.config.auth.LoginUser;
+import com.example.webservice.springboot.config.auth.dto.SessionUser;
 import com.example.webservice.springboot.service.posts.PostsService;
 import com.example.webservice.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
--- src/test/java/com/example/webservice/springboot/web/HelloControllerTest.java
+++ src/test/java/com/example/webservice/springboot/web/HelloControllerTest.java
@@ -1,11 +1,15 @@
 package com.example.webservice.springboot.web;
 
+import com.example.webservice.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -14,13 +18,16 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
-@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
+//@MockBean(JpaMetamodelMappingContext.class)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception {
 
         String hello = "hello";
@@ -30,6 +37,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "name";
         int amount = 1000;
@@ -37,7 +45,7 @@ public class HelloControllerTest {
                 .param("name", name)
                 .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name" , is(name)))
+                .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
 
 
